4efa1ae7d764387892715f36fc527cfb0a7b3d8a
Benjamin Tondase <benjamin1tondase211@gmail.com>
2024-08-01 08:35:05 +0100

    fix the problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // Ajoutez ici le domaine de votre image
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
   distDir: "build",
 };
