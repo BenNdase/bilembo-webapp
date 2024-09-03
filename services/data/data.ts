616af1429be2fd0abf4da2acafb51a1256118f8d
juju-heros <jbgbamo@gmail.com>
2024-09-03 17:45:47 +0200

    feat : add new data

--- services/data/data.ts
+++ services/data/data.ts
@@ -95,4 +95,47 @@ export const datas = [
       },
     ],
   },
+  {
+    id: 3,
+    slug: "prosper-kianonga",
+    profile_image:
+      "https://res.cloudinary.com/dwh18htmr/image/upload/v1722522135/Bilembo/simeon/simeon_7_mfdsv4.jpg",
+    name: "kianonga",
+    firstname: "prosper",
+    lastname: "Lukian",
+    birth: "15 Septembre 1962 ",
+    birthplace: "Matoto",
+    deceased: "18 Janvier 2019",
+    deceasedplace: "Kinshasa",
+    profession: "",
+    nationality: "Congolaise",
+    summary:
+      "Ingénieur Simeon Makila Lukian était connu pour sa détermination et son optimisme. Il était né dans une famille modeste en République Démocratique du Congo et avait une passion pour les sciences et la photographie. Il avait poursuivi ses études en électricité et avait travaillé à la Société Nationale d'Electricité (SNEL), où il s'était distingué par son travail acharné et sa passion pour l'amélioration des réseaux électriques.\n\n Il était également un chrétien catholique fervent et avait été une figure respectée dans sa communauté. Malgré une maladie grave, il avait continué à travailler et à se battre jusqu'à sa mort tragique en 2019",
+    youth_training:
+      "Dès son plus jeune âge, Simeon était un garçon curieux et intelligent, qui aimait apprendre et découvrir de nouvelles choses. Il a ainsi fréquenté l'école primaire SIA de la mission catholique SIA dans son territoire natal, où il s'est distingué par son excellence académique. Il avait une grande passion pour les mathématiques et la physique, et il aimait résoudre des équations et des problèmes complexes. \n\n Malgré les difficultés économiques auxquelles sa famille était confrontée, Simeon a toujours été soutenu dans ses études par ses parents, qui croyaient en son potentiel. Ils ont travaillé dur pour lui permettre de poursuivre ses rêves. \n\n Après avoir obtenu son diplôme d'état à l'institut KIKESA dans la ville de Bandundu, Simeon est retourné à la mission SIA en tant qu'enseignant de mathématiques et de physique au niveau humanitaire. Il aimait enseigner et partager ses connaissances avec les élèves, mais il avait également une passion pour la photographie. \n\n Un jour, alors qu'il prenait des photos dans les rues de Kinshasa, la capitale, il a été intrigué par les photographes professionnels qu'il a rencontrés et a décidé d'en apprendre davantage sur la photographie. Il a acheté des livres et des magazines sur le sujet, et a commencé à pratiquer en prenant des photos dans son temps libre. Simeon a travaillé dur pour perfectionner ses compétences en photographie, et il est rapidement devenu un photographe talentueux et reconnu dans la ville de Kinshasa. Ses photos ont été exposées dans des galeries d'art et ont été publiées dans des magazines locaux. \n\n *** De 2001 à 2003, il poursuit ses études pour l’obtention du diplôme de licence en génie électrique à l’ISTA. Il en ressort Ingénieur civil. Il travaille ensuite comme gestionnaire des réseaux dans un centre de vente et service de la SNEL (CVS/KINGABWA). \n\n *** En 2006, il perd son travail à la SNEL en raison de difficultés économiques rencontrées par l'entreprise. Il est contraint de rejoindre la Société de réseau et télécommunication TIGO, où il travaille pendant quelques années avant de rejoindre la société SOGEAC. \n\n *** En 2010, Simeon obtient sa réintégration à la SNEL au grade de cadre de collaboration. Il était très heureux de retrouver son entreprise et de pouvoir contribuer à son développement. Il a travaillé avec acharnement pour améliorer les réseaux électriques et assurer la satisfaction des clients. \n\n *** Simeon était un ingénieur talentueux et respecté, qui avait une grande passion pour son travail et pour aider les autres. Il était toujours prêt à aider ses collègues et à partager ses connaissances avec eux. Il était également très apprécié par ses clients pour son professionnalisme et sa gentillesse.",
+    career:
+      "Cependant, Simeon avait toujours une passion pour les sciences et l'électricité en particulier. Il a donc décidé de poursuivre ses études universitaires à l'Institut Supérieur des Techniques Appliquées (ISTA/N'DOLO) de Kinshasa, où il a étudié l'électricité de 1984 à 1987. Il a obtenu son diplôme de graduat en Électricité et a commencé son stage professionnel à la Société Nationale d'Electricité (SNEL). \n\n Simeon a rapidement fait ses preuves à la SNEL, où il a occupé un poste de chargé d'études adjoint. Il était passionné par son travail et était déterminé à faire une différence dans l'entreprise. Il a travaillé dur pour améliorer l'efficacité énergétique des réseaux électriques, ce qui a permis de réduire les coûts et d'améliorer la qualité de l'électricité fournie aux clients.",
+    cause_death:
+      "Malheureusement, à partir de 2012, Simeon est tombé malade. Il a été diagnostiqué avec une maladie grave qui a affecté sa santé et l'a empêché de travailler pendant de longues périodes. Malgré cela, il a continué à se battre et à faire face à la maladie avec courage et détermination. \n\n Il a été soutenu par sa famille, ses amis et ses collègues, qui ont fait de leur mieux pour l'aider dans cette période difficile. Il a continué à travailler autant que possible, mais sa santé a finalement décliné. \n\n Le 18 janvier 2019, à l'âge de 56 ans, Simeon a tragiquement perdu sa vie au centre hospitalier NGANDA à Kinshasa. Sa mort a été une grande perte pour sa famille, ses amis et ses collègues, qui ont tous été touchés par son optimisme, sa gentillesse et son dévouement. \n\n Simeon était un homme remarquable, qui a surmonté de nombreux obstacles pour réaliser ses rêves. Il était un modèle pour les autres, qui ont été inspirés par sa persévérance et sa détermination. Il était un mari aimant et un père dévoué, qui a toujours cherché à soutenir sa famille dans tout ce qu'elle faisait.",
+    images: [
+      "https://res.cloudinary.com/dwh18htmr/image/upload/v1722511796/Bilembo/simeon/simeon_2_goz94m.jpg",
+      "https://res.cloudinary.com/dwh18htmr/image/upload/v1722511796/Bilembo/simeon/simeon_1_fnslhe.jpg",
+      "https://res.cloudinary.com/dwh18htmr/image/upload/v1722511796/Bilembo/simeon/simeon_5_wgg6lw.jpg",
+      "https://res.cloudinary.com/dwh18htmr/image/upload/v1722511795/Bilembo/simeon/simeon_4_vpyx7o.jpg",
+      "https://res.cloudinary.com/dwh18htmr/image/upload/v1722511795/Bilembo/simeon/simeon_3_qi9fwv.jpg",
+    ],
+    epilogue:
+      "Le parcours de Simeon MAKILA LUKIAN est un exemple de détermination, de courage et d'optimisme. Il a surmonté de nombreux obstacles pour devenir un ingénieur respecté et talentueux, et il a inspiré de nombreuses personnes tout au long de sa vie. \n\n Ses réalisations ont été rendues possibles grâce à son travail acharné, sa passion pour l'apprentissage et sa détermination à réussir. Il a laissé un héritage durable pour sa famille, ses amis et ses collègues, qui se souviendront toujours de lui avec respect et affection.",
+    souvenirs: [
+      {
+        image:
+          "https://res.cloudinary.com/dwh18htmr/image/upload/v1718640298/asodec_p4agn1.png",
+        date: "20 Juin 1990",
+        title:
+          "Elu Pasteur de l’assemblée chrétienne de Righini, aujourd’hui Baruti Tabernacle.",
+        description:
+          "Quis quis consequat cillum cupidatat reprehenderit sit et ullamco incididunt ad et dolore velit tempor. Deserunt enim cillum veniam incididunt reprehenderit labore. Eu mollit laboris consequat eu aliqua. Dolor pariatur minim amet amet esse sit adipisicing est fugiat minim dolore. Commodo ullamco adipisicing ea sit reprehenderit enim qui elit ut aliquip esse esse pariatur sint. Aute ea et sunt consequat voluptate. Sunt occaecat ipsum minim esse exercitation do deserunt eu fugiat cillum veniam.",
+      },
+    ],
+  },
 ];
